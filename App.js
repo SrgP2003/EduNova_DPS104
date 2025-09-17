1802627dd30efe1b7d7c6a3dee2472607726f39c
Sergio Palacios <sergioarepalacios9264@gmail.com>
2025-09-16 21:43:45 -0600

    Configuracion inicial del proyecto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Configuracion inicial del proyecto e instalacion de lo basico para empezar</Text>
       <StatusBar style="auto" />
     </View>
   );
